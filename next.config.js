8bc2eebf714c1394dcbbde29fc9a1be7df4d6669
Bilal Orhanlar <bilal.orhanlar149@gmail.com>
2025-05-04 16:27:12 +0300

    Add CORS configuration

--- next.config.js
+++ next.config.js
@@ -10,7 +10,28 @@ const nextConfig = {
   },
   images: {
     domains: ['s3.amazonaws.com', 's3.eu-north-1.amazonaws.com'],
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
